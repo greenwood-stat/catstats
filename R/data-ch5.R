763374f0dde39a379cbd1dad9165e942866e91a2
kferris10 <kevin.ferris10@gmail.com>
2015-05-12 15:59:19 -0600

    Add mtfires data

--- R/data-ch5.R
+++ R/data-ch5.R
@@ -5,9 +5,27 @@
 #' officer measured their BAC.
 #'
 #' @format A data frame with 16 rows and 2 variables:
-#' \describe {
-#'  \item{Beers}{Number of cans of beer consumed}
-#'  \item{BAC}{Blood alcohol content, in grams per decaliter}
+#' \describe{
+#'  \item{Beers}{number of cans of beer consumed}
+#'  \item{BAC}{blood alcohol content, in grams per decaliter}
 #' }
 #' @source In progress
 "BB"
+
+#' Montana Fires
+#'
+#' This data frame contains information about yearly wildfires in Montana from
+#' 1985 to 2007.  Each year constitutes one observation in these data so there
+#' are 23 rows (one for each year).
+#'
+#' @format A data frame with 23 rows and 3 columns:
+#' \describe{
+#'  \item{Year}{year of data}
+#'  \item{Temperature}{average daily high temperature, in degrees Fahrenheit}
+#'  \item{Hectares}{total area burned by wildfires.  One hectare is equal to
+#'    10,000 square meters.}
+#'   }
+#' @source Gude, P.H., Cookson, A.J., Greenwood, M.C., and Haggerty, M. (2009)
+#'   Homes in Wildfire-Prone Areas:An Empirical Analysis of Wildfire Suppression
+#'   Costs and Climate Change.
+"mtfires"
