f06348797c0823a69fc581b6d87c7d1a53996839
kferris10 <kevin.ferris10@gmail.com>
2015-05-12 15:36:26 -0600

    Add BAC data

---
+++ R/data-ch5.R
@@ -0,0 +1,13 @@
+#' Blood Alcohol Contents
+#'
+#' A group of n=16 student volunteers at The Ohio State University drank a
+#' randomly assigned number of cans of beer. Thirty minutes later, a police
+#' officer measured their BAC.
+#'
+#' @format A data frame with 16 rows and 2 variables:
+#' \describe {
+#'  \item{Beers}{Number of cans of beer consumed}
+#'  \item{BAC}{Blood alcohol content, in grams per decaliter}
+#' }
+#' @source In progress
+"BB"
